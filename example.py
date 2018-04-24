4e50f5cd5cc8929d36ffe73ba4dc679ef2f3b9f1
randysuen1991 <randysuen1991@gmail.com>
2018-04-24 20:16:29 +0800

    new

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ import TradeBase
 def example1():
     trader = TradeBase.Trader()
     trader.Connect()
-    trader.Collect_Data_Ticker(delay_time=5,num_data=100)
+    trader.Collect_Data_Ticker(delay_time=10,num_data=300)
     trader.Start()
     
 def example2():
@@ -12,6 +12,15 @@ def example2():
     trader.Subscribe_Order_Book(crypto='XRP',money='USD',depth=10,subscribe=True,delay_time=1)
     trader.Start()
     
-    
+def visualize():
+    import  matplotlib.pyplot as plt
+    import os
+    import sys
+    import numpy as np
+    sys.path.append('C:\\Users\\ASUS\\Dropbox\\pycode\\mine\\Crypto-Currency-Trading\\data')
+    name = ['bch','btc','btg','dash','eth','xlm','xrp','zec']
+    for file, name in zip(os.listdir('C:\\Users\\ASUS\\Dropbox\\pycode\\mine\\Crypto-Currency-Trading\\data'),name):
+        print(file,name)
+        np.load(file,name)
 if __name__ == '__main__':
-    example1()
+    visualize()
--- strategies.py
+++ strategies.py
@@ -25,22 +25,25 @@ class PairTrading():
     def PairTrade(cls, crypto_x, crypto_y, price_x, price_y, amount, lm, sigma, account, **kwargs):
         num_sigma = kwargs.get('num_sigma',2)
         if np.log(price_y) - lm.predict(np.log(price_x)) > num_sigma * sigma :
-            yield from cls.Trade(amount,crypto_x,crypto_y,account,cond='over')
-        elif np.log(price_y) - lm.predict(np.log(price_x)) < num_sigma * sigma :
-            yield from cls.Trade(amount,crypto_x,crypto_y,account,cond='under')
+            yield from cls.Trade(amount,crypto_x,crypto_y,price_x,price_y,account,cond='over')
+        elif np.log(price_y) - lm.predict(np.log(price_x)) <= - num_sigma * sigma :
+            yield from cls.Trade(amount,crypto_x,crypto_y,price_x,price_y,account,cond='under')
             
             
+            
+            
+    # The action depends on the amount of the currency.
     @classmethod
     @asyncio.coroutine
-    def Trade(cls, amount, crypto_x, crypto_y, account, cond):
+    def Trade(cls, amount, crypto_x, crypto_y, price_x, price_y, account, cond):
         if cond == 'over' : 
             if account[crypto_y] > account[crypto_x] :
-                yield from WS.send(Request.Place_Order(crypto_y,amount,price,type='sell'))
+                yield from WS.send(Request.Place_Order(crypto_y,amount,price_y,type='sell'))
             else :
-                yield from WS.send(Request.Place_Order(crypto_x,amount,price,type='buy'))
+                yield from WS.send(Request.Place_Order(crypto_x,amount,price_x,type='buy'))
         elif cond == 'under' :
             if account[crypto_y] > account[crypto_x] :
-                yield from WS.send(Request.Place_Order(crypto_y,amount,price,type='buy'))
+                yield from WS.send(Request.Place_Order(crypto_y,amount,price_y,type='buy'))
             else :
-                yield from WS.send(Request.Place_Order(crypto_x,amount,price,type='sell'))
+                yield from WS.send(Request.Place_Order(crypto_x,amount,price_x,type='sell'))
             
