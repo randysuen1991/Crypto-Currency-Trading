c17b9ef2708ec66d18c7451eeee459c460f02849
randysuen1991 <randysuen1991@gmail.com>
2018-04-28 15:46:12 +0800

    new

--- Analysis.py
+++ Analysis.py
@@ -1,6 +1,10 @@
 import scipy.stats as stats
+import numpy as np
 
 class Analysis():
     def Spearman_Rank(X,Y):
         r_s = stats.spearmanr(X,Y)
         return r_s
+    def Correlation(X,Y):
+        c_c = np.corrcoef(X,Y)
+        return c_c
